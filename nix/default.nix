8038fd62850fbb450d2dd7a9eb1395bc98275fdc
Graham Christensen <graham@grahamc.com>
2017-12-22 12:37:41 -0500

    Update nixpkgs

--- nix/default.nix
+++ nix/default.nix
@@ -15,11 +15,6 @@ in import (hostpkgs.stdenv.mkDerivation {
   };
 
   patches = [
-    (fetchurl {
-      # aarch64 netboot patches
-      url = "https://github.com/NixOS/nixpkgs/commit/08b8bc24cb818d78971f6cb941b7991e54c6971b.patch";
-      sha256 = "0gzqx2wp6s5b6rkk4a9nqi3d9dx6bd0vc2i1sh2289z6dlghxzp0";
-    })
   ];
 
   moveToOut = ''
