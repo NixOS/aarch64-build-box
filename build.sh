b9963de3e4f16664c9ea002ad5b685983b8d79e2
Graham Christensen <graham@grahamc.com>
2019-05-30 20:00:47 -0400

    ssh: add SSHOPTS

--- build.sh
+++ build.sh
@@ -48,9 +48,9 @@ out=$(ssh $SSHOPTS "$buildHost" NIX_REMOTE=daemon nix-store --keep-going -r "$dr
 
 psk=$(head -c 9000 /dev/urandom | md5sum | awk '{print $1}')
 
-ssh "$pxeHost" rm -rf "${pxeDir}/${target}.old"
-ssh "$pxeHost" mv "${pxeDir}/${target}" "${pxeDir}/${target}.old"
-ssh "$pxeHost" -- nix-shell -p mbuffer openssl --run \
+ssh $SSHOPTS "$pxeHost" rm -rf "${pxeDir}/${target}.old"
+ssh $SSHOPTS "$pxeHost" mv "${pxeDir}/${target}" "${pxeDir}/${target}.old"
+ssh $SSHOPTS "$pxeHost" -- nix-shell -p mbuffer openssl --run \
     "openssl s_server -nocert -naccept 1 \
          -psk $psk -accept ${opensslPort} \
        | mbuffer | tar -C ${pxeDir}/${target} -zx"
