deffe3332d7c4049deb5219d205e7c9a9882979f
Graham Christensen <graham@grahamc.com>
2019-06-07 20:02:30 -0400

    Bump to 19.03

--- nix/update-nixpkgs.sh
+++ nix/update-nixpkgs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -i bash -p nix-prefetch-git
 
-REV=refs/heads/nixos-18.09
+REV=refs/heads/nixos-19.03
 
 nix-prefetch-git https://github.com/nixos/nixpkgs-channels.git \
                  --rev "$REV" > ./nix/nixpkgs.json
