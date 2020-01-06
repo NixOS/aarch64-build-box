8ae39466eabe07451a5f7047517196f6441411fb
Francesco Zanini <francesco@zanini.me>
2020-01-06 16:41:11 +0100

    Add zaninime as trusted user

--- users.nix
+++ users.nix
@@ -256,6 +256,11 @@ let
       trusted = true;
       keys = ./keys/yegortimoshenko;
     };
+
+    zaninime = {
+      trusted = true;
+      keys = ./keys/zaninime;
+    };
   };
 
   ifAttr = key: default: result: opts:
