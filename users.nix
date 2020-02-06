e0cce0eaf73cbc5d573e98bf8b3f3960ea273312
TQ Hirsch <thequux@thequux.com>
2020-02-06 22:35:10 +0100

    Add thequux as a trusted user

--- users.nix
+++ users.nix
@@ -217,6 +217,11 @@ let
       keys = ./keys/t184256;
     };
 
+    thequux = {
+      trusted = true;
+      keys = ./keys/thequux;
+    };
+
     timokau = {
       trusted = true;
       keys = ./keys/timokau;
