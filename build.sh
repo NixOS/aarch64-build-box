1e9e3ab1017015074ee2f915bb39672b22d2eb12
Graham Christensen <graham@grahamc.com>
2020-05-19 22:43:13 -0400

    Ref a nixpkgs for the build of the config

--- build.sh
+++ build.sh
@@ -63,7 +63,7 @@ trap cleanup EXIT
 
 set -eu
 
-drv=$(nix-instantiate ./configuration.nix --show-trace)
+drv=$(nix-instantiate -I nixpkgs=channel:nixos-unstable-small ./configuration.nix --show-trace)
 NIX_SSHOPTS=$SSHOPTS nix-copy-closure --to "$buildHost" "$drv"
 out=$(ssh $SSHOPTS "$buildHost" NIX_REMOTE=daemon nix-store --keep-going -r "$drv" -j 5 --cores 45 --add-root ./community-build-box --indirect)
 
