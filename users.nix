c334a5ecd75f53b2041aa126931ad1e4d47c25b6
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2022-01-18 21:33:42 +1100

    Add tomfitzhenry as trusted user
    This is useful for large aarch64 builds needed for the mobile-nixos
    contributions such as https://github.com/alsa-project/alsa-ucm-conf/pull/134.
    
    History of contributions: https://github.com/NixOS/mobile-nixos/pulls?q=is%3Apr+author%3Atomfitzhenry+is%3Aclosed

--- users.nix
+++ users.nix
@@ -328,6 +328,11 @@ let
       keys = ./keys/tomberek;
     };
 
+    tomfitzhenry = {
+      trusted = true;
+      keys = ./keys/tomfitzhenry;
+    };
+
     vcunat = {
       trusted = true;
       keys = ./keys/vcunat;
