6e6b4ec786a84333170b624dc02a17b54cc86b8c
hacker1024 <hacker1024@users.sourceforge.net>
2024-01-14 14:23:39 +1100

    add hacker1024 as trusted user

--- users.nix
+++ users.nix
@@ -221,6 +221,11 @@ let
       keys = ./keys/grahamc;
     };
 
+    hacker1024 = {
+      trusted = true;
+      keys = ./keys/hacker1024;
+    };
+
     hexa = {
       trusted = true;
       keys = ./keys/hexa;
