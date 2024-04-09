2b38403f4398f0d27433c2bf0647343ecc80c56e
Cole Helbling <cole.e.helbling@outlook.com>
2024-04-09 15:22:59 -0700

    fixup: point at newer ofborg build
    ...yikes.

--- nix/update-ofborg-path.sh
+++ nix/update-ofborg-path.sh
@@ -6,7 +6,7 @@ set -eu
 
 ofborg=$(curl \
              -L -H "Content-Type: application/json" \
-             https://hydra.nixos.org/job/ofborg/release/ofborg.rs.aarch64-linux/latest \
+             https://hydra.nixos.org/job/ofborg/release/buildRs.aarch64-linux/latest \
              | jq -r .buildoutputs.out.path)
 
 nix-store --add-root ./nix/ofborg-path --indirect -r "$ofborg"
