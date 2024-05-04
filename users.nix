548a4d5ea4639b511625178b107415f88dae7409
Jade Lovelace <software@lfcode.ca>
2024-05-03 19:45:20 -0700

    Add jade

--- users.nix
+++ users.nix
@@ -239,6 +239,11 @@ let
       keys = ./keys/izorkin;
     };
 
+    jade = {
+      trusted = true;
+      keys = ./keys/jade;
+    };
+
     janik = {
       trusted = true;
       keys = ./keys/janik;
